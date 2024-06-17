ce1bbba9789d85c168b1626add2056854469ef71
sex-male <cqg0112@gmail.com>
2024-06-17 11:50:28 +0900

    0.004
    movement2D add

--- Assets/Scripts/Movement2D.cs
+++ Assets/Scripts/Movement2D.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class Movement2D : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //private:
 }
